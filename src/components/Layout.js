b9921e302ed5307b458f4607eda595dba2b2a243
KHong <albtrossfacefall@gmail.com>
2020-03-27 19:28:33 -0400

    get modal to toggle with csstransition

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 
+import { CSSTransition } from 'react-transition-group';
+import "../styles/modalmenu.scss"
 import layoutStyles from "../styles/layout.module.scss";
 
-const Burger = () => (
+const Burger = ({toggleMenu}) => (
   <div 
     className = {layoutStyles.burger}
+    onClick = {toggleMenu}
   >
     <div/>
     <div/>
@@ -12,18 +15,25 @@ const Burger = () => (
   </div>
 )
 
-const SideMenu = () => (
-  <div>
-    <ul>
-      <li>foo</li>
-      <li>bar</li>
-    </ul>
-  </div>
+const SideMenu = ({ toggleMenu, menuActive }) => (
+  <>
+    {/* <div >
+      <ul></ul>
+    </div> */}
+    
+    <CSSTransition
+        in={menuActive}
+        timeout={200}
+        classNames="dark-modal"
+      >
+      <div onClick={ toggleMenu }/>
+    </CSSTransition>
+  </>
 )
 
-const Header = () => (
+const Header = ({ toggleMenu }) => (
   <div className={layoutStyles.header}>
-    <Burger />
+    <Burger toggleMenu={toggleMenu} />
     
     <div className={layoutStyles.titleContainer}>
       <h1>Co-Caring</h1>
@@ -32,9 +42,21 @@ const Header = () => (
 )
 
 const Layout = ({ children }) => {
+  const [menuActive, setMenuActive] = useState(false)
+  const toggleMenu = () => {
+    console.log(menuActive)
+    setMenuActive(!menuActive)
+  }
+
   return (
     <>
-      <Header />
+      <Header toggleMenu={toggleMenu} />
+      
+      <SideMenu 
+        menuActive={menuActive}
+        toggleMenu={toggleMenu}
+      />
+
       <main>{children}</main>
     </>
   )

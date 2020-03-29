0b9789034ee78e8b3c550bda70c94520f06ed941
KHong <albtrossfacefall@gmail.com>
2020-03-29 19:19:14 -0400

    begin building out and implementing resources page

---
+++ src/components/ResourcesComponents.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import resourceStyles from '../styles/resources-components.module.scss';
+
+export const TipList = ({children}) => (
+  <div className={resourceStyles.tipListContainer}>
+    {children}
+  </div>
+)
+
+export const TipItem = ({imgClass, title, copy, width, textSide}) => (
+  
+  <div className={resourceStyles.tipContainer}>
+    
+    {textSide==='right' ? 
+      <>
+        <div className={resourceStyles[imgClass]}/>
+
+        <div className={resourceStyles.text} style={{width: width}}>
+          <h4>{title}</h4>
+          <p>{copy}</p>
+        </div>
+      </>
+    :
+      <>
+        <div className={resourceStyles.text}>
+          <h4>{title}</h4>
+          {copy}
+        </div>
+        
+        <div className={resourceStyles[imgClass]}/>
+      </>
+    }
+
+  </div>
+)
+
+export const TrendingList = ({children}) => (
+  <div className={resourceStyles.trendingListContainer}>
+    {children}
+  </div>
+)
+
+export const TrendingItem = ({imgClass, title, copy, url}) => (
+  <div className={resourceStyles.trendingItemContainer}>
+    <a href={url}>
+      <div className={resourceStyles[imgClass]}/>
+
+      <h5>{title}</h5>
+      <p>{copy}</p>
+    </a>
+  </div>
+)
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -3,13 +3,14 @@ import React from 'react'
 import TopBanner from '../components/TopBanner';
 import TextSection from '../components/TextSection';
 import TextDivider from '../components/TextDivider';
+import { TipList, TipItem, TrendingList, TrendingItem } from '../components/ResourcesComponents';
 
 const Resources = () => {
   return (
     <div>
       <TopBanner 
-        title='Resources'
-        copy='Review our tips on how to #shelterinplace.'
+        title = 'Resources'
+        copy = 'Review our tips on how to #shelterinplace.'
         button = {false}
         imgClass = 'resources'
       />
@@ -20,10 +21,56 @@ const Resources = () => {
 
       <TextDivider/>
 
+
       <TextSection title='Our Top Tips'/>
+      <TipList>
+        <TipItem 
+          imgClass = 'tip1Exercise'
+          title = 'Exercise'
+          copy = 'Seek moments of growth to challenge yourself. Find a workout video online, do some home yoga, or go for a walk around your neighborhood while sticking to the 6 feet apart rule.'
+          width = '530px'
+          textSide = 'right'
+        />
+        
+        {/* <TipItem 
+          imgClass = {}
+          title = {}
+          copy = {}
+          textSide = 'left'
+        />
+
+        <TipItem 
+          imgClass = {}
+          title = {}
+          copy = {}
+          textSide = 'right'
+        /> */}
+      </TipList>
+
 
       <TextSection title="What's Trending"/>
+      <TrendingList>
+        {/* <TrendingItem 
+          imgClass = {}
+          title = {}
+          copy = {}
+          url = {}
+        /> */}
 
+        {/* <TrendingItem 
+          imgClass = {}
+          title = {}
+          copy = {}
+          url = {}
+        />
+
+        <TrendingItem 
+          imgClass = {}
+          title = {}
+          copy = {}
+          url = {}
+        /> */}
+      </TrendingList>
       {/* <ol>
         <li><strong>Exercise</strong> – Seek moments of growth to challenge yourself. Find a workout video online, do some home yoga, or go for a walk around your neighborhood while sticking to the 6 feet apart rule.</li>
         <li><strong>Social Connection</strong> – Just because we’re sheltering doesn’t mean we can’t stay connected. Find new ways to keep in touch with your community and friends. If you’re a texter, hop on the phone for a call. If you miss seeing your friends faces, try out Skype, FaceTime, Facebook Messenger, or Zoom. The options are plenty!</li>

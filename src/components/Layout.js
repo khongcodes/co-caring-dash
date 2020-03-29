8d2bf968b49acd75a77b76b86735e9bb533d57a7
KHong <albtrossfacefall@gmail.com>
2020-03-28 23:29:44 -0400

    remove side menu from home render

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,55 +1,29 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-import SideMenu from './SideMenu';
-
-import "../styles/modalmenu.scss"
+// import "../styles/modalmenu.scss"
 import layoutStyles from "../styles/layout.module.scss";
-
-const Burger = ({menuActive, toggleMenu}) => (
-  <div 
-    className = { menuActive ? layoutStyles.burgerActive : layoutStyles.burger }
-    onClick = {toggleMenu}
-  >
-    <div/>
-    <div/>
-    <div/>
-  </div>
-)
 
 const Header = ({ menuActive, toggleMenu }) => (
   <div className={layoutStyles.header}>
-    <Burger 
-      menuActive = {menuActive}
-      toggleMenu = {toggleMenu} 
-    />
     
     <div className={layoutStyles.titleContainer}>
       <Link to='/'>
         <h1>Co-Caring</h1>
       </Link>
     </div>
+
+    <div className={layoutStyles.navLinkContainer}>
+
+    </div>
   </div>
 )
 
 const Layout = ({ children }) => {
-  const [menuActive, setMenuActive] = useState(false)
-  const toggleMenu = () => {
-    console.log(menuActive)
-    setMenuActive(!menuActive)
-  }
 
   return (
     <>
-      <Header 
-        menuActive = {menuActive}
-        toggleMenu = {toggleMenu} 
-      />
-
-      <SideMenu 
-        menuActive = {menuActive}
-        toggleMenu = {toggleMenu}
-      />
+      <Header />
 
       <main>{children}</main>
     </>
